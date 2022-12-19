6eee3ad9b42663089b6e98a23f13050b4c1c245a
mbuhendwa <mkbuhendwa@gmail.com>
2022-12-19 18:44:12 +0100

    feat: added course card component

---
+++ client/src/components/CourseCard/course-card.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import envData from '../../../../config/env.json';
+import PlayIcon from '../../assets/images/play.svg';
+
+import Map from '../Map/index';
+
+import './course-card.css';
+
+const { apiLocation } = envData;
+
+interface LandingDetailsProps {
+  isSignedIn?: boolean;
+  isAvailable: boolean;
+  description: string;
+  title: string;
+  icon: string;
+  alt?: string;
+}
+
+const CourseCard = ({
+  isSignedIn,
+  isAvailable,
+  description,
+  title,
+  icon,
+  alt
+}: LandingDetailsProps): JSX.Element => {
+  return (
+    <div className='card-course-detail-back standard-radius-5 card-outlin-border bg-light'>
+      <div className='card-course-detail-unit position-relative'>
+        <div className='card-outlin-border bg-light standard-radius-5'>
+          <div className='card-course-detail-header hide-small'>
+            <div className='card-course-detail-logo push'>
+              <img src={icon} alt={alt} className='img-fluid' />
+            </div>
+          </div>
+          <div className='card-course-detail-item'>
+            <h4 className='fw-bold text-love-light card-title-w60'>{title}</h4>
+          </div>
+          <div className='card-course-detail-item  flexible'>
+            <p className='text-responsive'>{description}</p>
+          </div>
+          <div className='card-course-detail-footer'>
+            <div className='push'>
+              {isAvailable ? (
+                isSignedIn ? (
+                  <Map
+                    forLanding={true}
+                    single={true}
+                    className='link-course text-love-light fw-semi-bold text-responsive'
+                    keyPrefix='landing-details'
+                  >
+                    <div className='row-link'>
+                      <div className='row-link-text'>{'Suivre le cours  '}</div>
+                      <div>
+                        <img
+                          src={PlayIcon}
+                          alt='Laptop icon'
+                          className='play'
+                        />
+                      </div>
+                    </div>
+                  </Map>
+                ) : (
+                  <a
+                    href={`${apiLocation}/signin`}
+                    className='link-course text-love-light fw-semi-bold text-responsive'
+                  >
+                    <div className='row-link'>
+                      <div className='row-link-text'>{'Suivre le cours  '}</div>
+                      <div>
+                        <img
+                          src={PlayIcon}
+                          alt='Laptop icon'
+                          className='play'
+                        />
+                      </div>
+                    </div>
+                  </a>
+                )
+              ) : (
+                <span className='push text-love-light fw-semi-bold text-responsive'>
+                  Bientôt disponible
+                </span>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+CourseCard.displayName = 'CourseCard';
+export default CourseCard;
--- client/src/components/landing/what-will-you-learn/what-will-you-learn.tsx
+++ client/src/components/landing/what-will-you-learn/what-will-you-learn.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import envData from '../../../../../config/env.json';
 import { Spacer } from '../../../components/helpers';
-import Map from '../../Map/index';
 import LaptopIcon from '../../../assets/images/laptop.svg';
 import AlgoIcon from '../../../assets/images/algorithmIcon.svg';
-import PlayIcon from '../../../assets/images/play.svg';
 
 import './what-will-you-learn.css';
+import CourseCard from '../../CourseCard/course-card';
 
-const { apiLocation } = envData;
 interface LandingDetailsProps {
   isSignedIn?: boolean;
 }
@@ -26,118 +23,29 @@ const WhatWillYouLearn = ({ isSignedIn }: LandingDetailsProps): JSX.Element => {
         </p>
       </div>
       <Spacer size={2} />
-      <div className=''>
-        <div className='card-course-detail-container'>
-          <div className='card-course-detail-back standard-radius-5 card-outlin-border bg-light'>
-            <div className='card-course-detail-unit position-relative'>
-              <div className='card-outlin-border bg-light standard-radius-5'>
-                <div className='card-course-detail-header hide-small'>
-                  <div className='card-course-detail-logo push'>
-                    <img
-                      src={LaptopIcon}
-                      alt='LearnCode'
-                      className='img-fluid'
-                    />
-                  </div>
-                </div>
-                <div className='card-course-detail-item'>
-                  <h4 className='fw-bold text-love-light card-title-w60'>
-                    {`Responsive`}
-                    <br />
-                    {`Web Design`}
-                  </h4>
-                </div>
-                <div className='card-course-detail-item  flexible'>
-                  <p className='text-responsive'>
-                    {`
+      <div className='card-course-detail-container'>
+        <CourseCard
+          icon={LaptopIcon}
+          isAvailable={true}
+          isSignedIn={isSignedIn}
+          description={`
                 Dans cette formation, tu apprendras les langages que les développeurs 
                 utilisent pour créer des pages Web : HTML (Hypertext Markup Language) 
                 pour le contenu, et CSS (Cascading Style Sheets) pour la conception. 
                 Enfin, tu apprendras à créer des pages Web adaptées à différentes tailles d'écran.
                 `}
-                  </p>
-                </div>
-                <div className='card-course-detail-footer'>
-                  <div className='push'>
-                    {isSignedIn ? (
-                      <Map
-                        forLanding={true}
-                        single={true}
-                        className='link-course text-love-light fw-semi-bold text-responsive'
-                        keyPrefix='landing-details'
-                      >
-                        <div className='row-link'>
-                          <div className='row-link-text'>
-                            {'Suivre le cours  '}
-                          </div>
-                          <div>
-                            <img
-                              src={PlayIcon}
-                              alt='Laptop icon'
-                              className='play'
-                            />
-                          </div>
-                        </div>
-                      </Map>
-                    ) : (
-                      <a
-                        href={`${apiLocation}/signin`}
-                        className='link-course text-love-light fw-semi-bold text-responsive'
-                      >
-                        <div className='row-link'>
-                          <div className='row-link-text'>
-                            {'Suivre le cours  '}
-                          </div>
-                          <div>
-                            <img
-                              src={PlayIcon}
-                              alt='Laptop icon'
-                              className='play'
-                            />
-                          </div>
-                        </div>
-                      </a>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className='card-course-detail-back standard-radius-5 card-outlin-border bg-light'>
-            <div className='card-course-detail-unit position-relative'>
-              <div className='card-outlin-border bg-light standard-radius-5'>
-                <div className='card-course-detail-header hide-small'>
-                  <div className='card-course-detail-logo push'>
-                    <img src={AlgoIcon} alt='LearnCode' className='img-fluid' />
-                  </div>
-                </div>
-                <div className='card-course-detail-item'>
-                  <h4 className='fw-bold text-love-light card-title-w60'>
-                    {`JavaScript Algorithms`}
-                    <br />
-                    {`and Data Structures`}
-                  </h4>
-                </div>
-                <div className='card-course-detail-item  flexible'>
-                  <p className='text-responsive'>
-                    {`
-                Alors que HTML et CSS contrôlent le contenu et le style  d'une page, 
+          title={`Responsive Web Design`}
+        />
+        <CourseCard
+          icon={AlgoIcon}
+          isAvailable={false}
+          isSignedIn={isSignedIn}
+          description={`Alors que HTML et CSS contrôlent le contenu et le style  d'une page, 
                 JavaScript est utilisé pour la rendre interactive. Dans le cadre de 
                 la certification JavaScript Algorithm and Data Structures, tu apprendras 
-                les principes fondamentaux de JavaScript, etc.
-                `}
-                  </p>
-                </div>
-                <div className='card-course-detail-footer'>
-                  <span className='push text-love-light fw-semi-bold text-responsive'>
-                    Bientôt disponible
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+                les principes fondamentaux de JavaScript, etc.`}
+          title={`JavaScript Algorithms and Data Structures`}
+        />
       </div>
     </div>
   );
