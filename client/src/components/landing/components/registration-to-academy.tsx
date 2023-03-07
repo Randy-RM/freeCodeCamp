0e906f6e8ad95f598631f3db08ccfb7d8678832e
Randy-RM <randymuhema@gmail.com>
2023-03-07 12:13:08 +0100

    refactor : Change occurrences of KADEA EDUCATION to KADEA ACADEMY

--- client/src/components/landing/components/registration-to-academy.tsx
+++ client/src/components/landing/components/registration-to-academy.tsx
@@ -10,7 +10,7 @@ const RegistrationToAcademy = (): JSX.Element => {
       <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
         <div>
           <h2 className='big-heading text-ligth'>
-            {`Inscris-toi à KADEA EDUCATION,`}
+            {`Inscris-toi à KADEA ACADEMY,`}
             <br />
             {`une formation qui change une vie.`}
           </h2>
