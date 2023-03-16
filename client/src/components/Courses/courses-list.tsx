00f2959df756d44927d3f039a14e5c286142850d
Joseph Kabemba <josephkabemba88@outlook.com>
2023-03-16 18:42:18 +0100

    fix(intro): translate ES6 introduction

--- client/src/components/Courses/courses-list.tsx
+++ client/src/components/Courses/courses-list.tsx
@@ -16,7 +16,7 @@ const CoursesList = ({ isSignedIn }: CoursesListProps): JSX.Element => {
         <br />
         <p className='text-responsive'>
           {`
-          Concentre-toi sur ce qui est nécessaire pour acquérir une compétence spécifique et applicable. 
+          Concentre-toi sur ce qui est nécessaire pour acquérir une compétence spécifique et applicable.
           Tu seras mieux outillé pour construire une carrière.
           `}
         </p>
@@ -30,9 +30,9 @@ const CoursesList = ({ isSignedIn }: CoursesListProps): JSX.Element => {
           title={`Responsive Web Design`}
           buttonText={`Suivre le cours  `}
           description={`
-                Dans cette formation, tu apprendras les langages que les développeurs 
-                utilisent pour créer des pages Web : HTML (Hypertext Markup Language) 
-                pour le contenu, et CSS (Cascading Style Sheets) pour la conception. 
+                Dans cette formation, tu apprendras les langages que les développeurs
+                utilisent pour créer des pages Web : HTML (Hypertext Markup Language)
+                pour le contenu, et CSS (Cascading Style Sheets) pour la conception.
                 Enfin, tu apprendras à créer des pages Web adaptées à différentes tailles d'écran.
                 `}
         />
@@ -41,9 +41,9 @@ const CoursesList = ({ isSignedIn }: CoursesListProps): JSX.Element => {
           isAvailable={false}
           isSignedIn={isSignedIn}
           title={`JavaScript Algorithms and Data Structures`}
-          description={`Alors que HTML et CSS contrôlent le contenu et le style  d'une page, 
-                JavaScript est utilisé pour la rendre interactive. Dans le cadre de 
-                la certification JavaScript Algorithm and Data Structures, tu apprendras 
+          description={`Alors que HTML et CSS contrôlent le contenu et le style  d'une page,
+                JavaScript est utilisé pour la rendre interactive. Dans le cadre de
+                la certification JavaScript Algorithm and Data Structures, tu apprendras
                 les principes fondamentaux de JavaScript, etc.`}
         />
         <CourseCard
@@ -52,9 +52,9 @@ const CoursesList = ({ isSignedIn }: CoursesListProps): JSX.Element => {
           title={`AWS Cours`}
           buttonText={`Suivre le cours  `}
           link={`/aws-courses`}
-          description={`Ce cours est conçu pour montrer aux participants comment 
-                  optimiser l'utilisation du cloud AWS grâce à la compréhension 
-                  de ces nombreux services et de leur intégration dans la création 
+          description={`Ce cours est conçu pour montrer aux participants comment
+                  optimiser l'utilisation du cloud AWS grâce à la compréhension
+                  de ces nombreux services et de leur intégration dans la création
                   de solutions basées sur le cloud.`}
         />
       </div>
