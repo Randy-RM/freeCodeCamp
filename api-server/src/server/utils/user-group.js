c52ed7ca12ab83adad62522b583029a677eb3c6c
Randy-RM <randymuhema@gmail.com>
2023-06-20 13:40:23 +0200

    feat : add insert user group and get all user group feature

--- api-server/src/server/utils/user-group.js
+++ api-server/src/server/utils/user-group.js
@@ -1,19 +1,82 @@
 import { isEmpty } from 'lodash';
 import loopback from 'loopback';
 
-export function insertUserGroup(
+export async function insertUserGroup(
   userGroup,
-  UserGroup = loopback.getModelByType('UserGroup')
+  UserGroup = loopback.getModelByType('userGroup')
 ) {
-  console.log('******* Sucèss : Group inserted *******');
+  try {
+    const userGroupExist = await UserGroup.find({ where: userGroup });
+    if (userGroupExist && userGroupExist.length > 0) {
+      throw new Error('A group already exists with this name');
+    }
+    const newUserGroup = await UserGroup.create(userGroup);
+    return {
+      userGroup: newUserGroup,
+      error: null
+    };
+  } catch (error) {
+    return {
+      userGroup: null,
+      error: error.message
+    };
+  }
+}
+
+export function getAllUserGroup(
+  page,
+  limit,
+  filter,
+  UserGroup = loopback.getModelByType('userGroup')
+) {
   return new Promise((resolve, reject) => {
-    UserGroup.insert(userGroup, (err, instance) => {
-      if (err || isEmpty(instance)) {
-        console.log('******* Error : Group is not inserted *******');
-        return reject(err || 'Error on insert user group');
-      }
-      console.log('******* Sucèss : Group inserted *******');
-      return resolve(instance);
-    });
+    if (filter) {
+      UserGroup.find(
+        {
+          where: filter,
+          skip: (page - 1) * limit,
+          limit: limit * 1
+        },
+        (err, instance) => {
+          if (err || isEmpty(instance)) {
+            return reject(err || 'No users group found');
+          }
+          return resolve(instance);
+        }
+      );
+    } else {
+      UserGroup.find(
+        { skip: (page - 1) * limit, limit: limit * 1 },
+        (err, instance) => {
+          if (err || isEmpty(instance)) {
+            return reject(err || 'No users group found');
+          }
+          return resolve(instance);
+        }
+      );
+    }
+  });
+}
+
+export function countUserGroupDocuments(
+  filter,
+  UserGroup = loopback.getModelByType('userGroup')
+) {
+  return new Promise((resolve, reject) => {
+    if (filter) {
+      UserGroup.find({ where: filter }, (err, count) => {
+        if (err || isEmpty(count)) {
+          return reject(err || 'can not count user group collection');
+        }
+        return resolve(count);
+      });
+    } else {
+      UserGroup.find((err, count) => {
+        if (err || isEmpty(count)) {
+          return reject(err || 'can not count user group collection');
+        }
+        return resolve(count);
+      });
+    }
   });
 }
