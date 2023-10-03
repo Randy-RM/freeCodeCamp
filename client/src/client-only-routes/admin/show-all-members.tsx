6bd5d45e042c391dcce6b2b5bff260b2e0708976
Joel Tondozi <joeltondozi@gmail.com>
2023-10-03 10:10:54 +0100

    fix : change limit of table memebre on 10

--- client/src/client-only-routes/admin/show-all-members.tsx
+++ client/src/client-only-routes/admin/show-all-members.tsx
@@ -121,7 +121,7 @@ export function ShowAllMembers(props: ShowAllMembersProps): JSX.Element {
   const getMembers = async () => {
     const memberList = await getDatabaseResource<UserList>(
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      `/all-users?page=${currentPage}&limit=2&classRoom=${groupMembers}&memberName=${memberNameToSearch}`
+      `/all-users?page=${currentPage}&limit=10&classRoom=${groupMembers}&memberName=${memberNameToSearch}`
     );
     if (memberList != null && !('error' in memberList)) {
       setMembers(memberList.userList);
