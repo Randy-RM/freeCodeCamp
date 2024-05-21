6ee24446620bc5d806145b050a81c10a1d1ce838
joetdz <joeltondozi@gmail.com>
2024-05-22 00:06:14 +0100

    feat: get moodle course by category in landing

--- client/src/components/landing/what-will-you-learn/filter/course-filter.tsx
+++ client/src/components/landing/what-will-you-learn/filter/course-filter.tsx
@@ -1,24 +1,68 @@
 import React, { useState } from 'react';
 import '../filter/course-filter.css';
-import { MoodleCourseCategory } from '../../../../client-only-routes/show-courses';
+import {
+  MoodleCourse,
+  MoodleCourseCategory,
+  MoodleCoursesCatalogue
+} from '../../../../client-only-routes/show-courses';
+import { getExternalResource } from '../../../../utils/ajax';
+import envData from '../../../../../../config/env.json';
+import { splitArray } from '../../../helpers';
+import sortCourses from '../../../helpers/sort-course';
+
+type MoodleCoursesFiltered = {
+  courses: MoodleCourse[] | null;
+  warnings: [];
+};
+const { moodleApiBaseUrl, moodleApiToken } = envData;
 
 const CourseFilterList = ({
-  courseCategories
+  courseCategories,
+  setMoodleCourses,
+  setIsDataOnLoading
 }: {
   courseCategories: MoodleCourseCategory[] | undefined | null;
+  setMoodleCourses: React.Dispatch<
+    React.SetStateAction<MoodleCoursesCatalogue | null | undefined>
+  >;
+  setIsDataOnLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }): JSX.Element => {
-  const [activeButton, setActiveButton] = useState<string>('Populaires'); // Initial active button
-  console.log('active log', activeButton == 'Populaires');
+  const [currentCategory, setCurrentCategory] = useState<string>('Populaires'); // Initial active button
+
+  console.log('active log', currentCategory == 'Populaires');
+  const filterByCategory = async (categoryId: number) => {
+    setIsDataOnLoading(true);
+    const moodleCourseFiltered: MoodleCoursesFiltered | null =
+      await getExternalResource<MoodleCoursesFiltered>(
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        `${moodleApiBaseUrl}?wstoken=${moodleApiToken}&wsfunction=core_course_get_courses_by_field&field=category&value=${categoryId}&moodlewsrestformat=json`
+      );
+    setIsDataOnLoading(false);
+
+    const splitCourses: MoodleCoursesCatalogue | null | undefined =
+      moodleCourseFiltered?.courses != null
+        ? splitArray<MoodleCourse>(
+            moodleCourseFiltered.courses.filter(
+              moodleCourse => moodleCourse.visible == 1
+            ),
+            4
+          )
+        : null;
+    //Order courses by their publication date
+    const sortedCourses = sortCourses(splitCourses);
+
+    setMoodleCourses(sortedCourses);
+  };
 
   const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setActiveButton(e.currentTarget.textContent as string); // Update active state based on clicked button text
+    setCurrentCategory(e.currentTarget.textContent as string); // Update active state based on clicked button text
   };
   return (
     <div className='landing-filter-container'>
       <ul className='filter-categories-list'>
         <button
           className={`category-name ${
-            activeButton === 'Populaires' ? 'active-button' : ''
+            currentCategory === 'Populaires' ? 'active-button' : ''
           }`}
           onClick={handleButtonClick}
         >
@@ -26,7 +70,7 @@ const CourseFilterList = ({
         </button>
         <button
           className={`category-name ${
-            activeButton === 'Programmation' ? 'active-button' : ''
+            currentCategory === 'Programmation' ? 'active-button' : ''
           }`}
           onClick={handleButtonClick}
         >
@@ -34,36 +78,23 @@ const CourseFilterList = ({
         </button>
         <button
           className={`category-name ${
-            activeButton === 'AWS' ? 'active-button' : ''
+            currentCategory === 'AWS' ? 'active-button' : ''
           }`}
           onClick={handleButtonClick}
         >
           {'AWS'}
-        </button>
-        <button
-          className={`category-name ${
-            activeButton === ' Marketing digital' ? 'active-button' : ''
-          }`}
-          onClick={handleButtonClick}
-        >
-          {' Marketing digital'}
-        </button>
-        <button
-          className={`category-name ${
-            activeButton === 'IA' ? 'active-button' : ''
-          }`}
-          onClick={handleButtonClick}
-        >
-          {'IA'}
         </button>
 
         {courseCategories?.map((element, index) => {
           return (
             <button
               className={`category-name ${
-                activeButton == element?.name ? 'active-button' : ''
+                currentCategory == element?.name ? 'active-button' : ''
               }`}
-              onClick={handleButtonClick}
+              onClick={() => {
+                handleButtonClick;
+                void filterByCategory(element?.id);
+              }}
               key={index}
             >
               {element?.name}
