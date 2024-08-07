fdff283dabbce465c94c2ebc90a5646cf8172824
Jumvi <judahmvi@gmail.com>
2024-08-07 22:58:15 +0100

    fix:add card cours language and refactor style

---
+++ client/src/utils/routes.ts
@@ -0,0 +1,10 @@
+const routes = {
+  learningPath: {
+    index: '/learning-path',
+    aws: '/learning-path/aws-courses',
+    moodle: '/learning-path/:category/:categoryId',
+    fullstack: '/learning-path/developpement-web'
+  }
+};
+
+export default routes;
