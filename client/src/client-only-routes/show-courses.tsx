0d6ce24636dbc502e0c319a557e98a0f840aad9c
Jumvi <judahmvi@gmail.com>
2024-08-07 22:51:39 +0100

    fix:add card cours language and refactor style

--- client/src/client-only-routes/show-courses.tsx
+++ client/src/client-only-routes/show-courses.tsx
@@ -17,11 +17,11 @@ import envData from '../../../config/env.json';
 import CourseCard from '../components/CourseCard/course-card';
 import LaptopIcon from '../assets/images/laptop.svg';
 import AlgoIcon from '../assets/images/algorithmIcon.svg';
-import PhBookBookmark from '../assets/images/ph-book-bookmark-thin.svg';
+// import PhBookBookmark from '../assets/images/ph-book-bookmark-thin.svg';
 import awsLogo from '../assets/images/aws-logo.png';
 
 import LaediesActIcon from '../assets/images/partners/we-act-logo.png';
-import NewBadge from '../assets/images/new.png';
+// import NewBadge from '../assets/images/new.png';
 import { createFlashMessage } from '../components/Flash/redux';
 import {
   Loader,
@@ -115,6 +115,8 @@ export type RavenCourse = {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   long_description: string;
   // eslint-disable-next-line @typescript-eslint/naming-convention
+  category?: Category[];
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   skill_level: string;
 };
 interface RavenTokenData {
@@ -124,6 +126,29 @@ interface RavenTokenData {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   valid_to: string;
 }
+
+type Tag = {
+  title: string;
+};
+
+type Category = {
+  tags?: Tag[];
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type Course = {
+  name: string;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  launch_url: string;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  short_description: string;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  long_description?: string;
+  duration: number;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  skill_level: string;
+  category?: Category[];
+};
 
 export interface RavenFetchCoursesDto {
   apiKey: string;
@@ -377,9 +402,9 @@ export function Courses(props: CoursesProps): JSX.Element {
     });
   }
 
-  const formatdate = (data: number) => {
-    return new Date(data * 1000).toLocaleDateString();
-  };
+  // const formatdate = (data: number) => {
+  //   return new Date(data * 1000).toLocaleDateString();
+  // };
 
   useEffect(() => {
     if (screenWidth > 990) setShowFilter(true);
@@ -397,7 +422,6 @@ export function Courses(props: CoursesProps): JSX.Element {
 
   return (
     <>
-      {/* <Helmet title={`${t('buttons.settings')} | Code Learning Plateform`} /> */}
       <Helmet title={`Nos cours | Kadea Online`} />
       <Grid className='bg-light'>
         <main>
@@ -408,7 +432,7 @@ export function Courses(props: CoursesProps): JSX.Element {
               onClick={() => {
                 setShowFilter(e => !e);
               }}
-              className=' show-filter-button '
+              className='show-filter-button'
             >
               <span>Filtrer</span>
               <svg
@@ -421,9 +445,8 @@ export function Courses(props: CoursesProps): JSX.Element {
                   <path fill='none' d='M0 0h24v24H0z' />
                   <path d='M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z' />
                 </g>
-              </svg>{' '}
+              </svg>
             </button>
-            {/* <Spacer /> */}
 
             <div className='card-filter-container'>
               {showFilter && (
@@ -434,7 +457,6 @@ export function Courses(props: CoursesProps): JSX.Element {
                   setMoodleCourses={setMoodleCourses}
                   setShowFilter={setShowFilter}
                   setIsDataOnLoading={setIsDataOnLoading}
-                  // setProgrammingCategory={setProgrammingCategory}
                   courseCategories={courseCategories}
                   currentCategory={currentCategory}
                   setCurrentCategory={setCurrentCategory}
@@ -444,13 +466,12 @@ export function Courses(props: CoursesProps): JSX.Element {
 
               <div className='card-courses-detail-container'>
                 <div>
-                  <h2 className='big-subheading'>{`Explorer notre catalogue`}</h2>
+                  <h2 className='big-subheading'>Explorer notre catalogue</h2>
                 </div>
                 <Spacer />
 
                 <CoursesCategoryCard
                   courseCategories={courseCategories}
-                  // getRavenResourcesPath={getRavenResourcesPath}
                   setRavenPath={setRavenPath}
                   setCurrentCategory={setCurrentCategory}
                   currentCategory={currentCategory}
@@ -460,6 +481,7 @@ export function Courses(props: CoursesProps): JSX.Element {
                   setMoodleCourses={setMoodleCourses}
                   setRavenCourses={setRavenCourses}
                 />
+
                 <div className='course__number'>
                   <p>Parcourir le catalogue complet</p>
                   <span>
@@ -470,12 +492,14 @@ export function Courses(props: CoursesProps): JSX.Element {
                     cours
                   </span>
                 </div>
+
                 {!isDataOnLoading ? (
                   <div className='card-course-detail-container'>
                     {currentPage == 1 &&
                       (currentCategory == null || currentCategory == -1) && (
                         <>
                           <CourseCard
+                            language='French'
                             icon={LaptopIcon}
                             sponsorIcon={LaediesActIcon}
                             alt=''
@@ -483,110 +507,115 @@ export function Courses(props: CoursesProps): JSX.Element {
                             phone={phone}
                             isAvailable={true}
                             isSignedIn={isSignedIn}
-                            title={`Responsive Web Design`}
-                            buttonText={`Suivre le cours  `}
-                            link={'/learn/responsive-web-design/'}
+                            title='Responsive Web Design'
+                            buttonText='Suivre le cours'
+                            link='/learn/responsive-web-design/'
                             description={`
-                  Dans ce cours, tu apprendras les langages que les développeurs 
-                  utilisent pour créer des pages Web : HTML (Hypertext Markup Language) 
-                  pour le contenu, et CSS (Cascading Style Sheets) pour la conception. 
-                  Enfin, tu apprendras à créer des pages Web adaptées à différentes tailles d'écran.
-                  `}
+                          Dans ce cours, tu apprendras les langages que les développeurs
+                          utilisent pour créer des pages Web : HTML (Hypertext Markup Language)
+                          pour le contenu, et CSS (Cascading Style Sheets) pour la conception.
+                          Enfin, tu apprendras à créer des pages Web adaptées à différentes tailles d'écran.
+                        `}
                           />
                           <CourseCard
+                            language='French'
                             icon={AlgoIcon}
                             alt=''
                             isAvailable={true}
                             isSignedIn={isSignedIn}
                             phone={phone}
                             name={name}
-                            title={`JavaScript Algorithms and Data Structures`}
-                            buttonText={`Suivre le cours  `}
-                            link={`/learn/javascript-algorithms-and-data-structures`}
-                            description={`Alors que HTML et CSS contrôlent le contenu et le style  d'une page, 
-                  JavaScript est utilisé pour la rendre interactive. Dans le cadre du 
-                  cours JavaScript Algorithm and Data Structures, tu apprendras 
-                  les principes fondamentaux de JavaScript, etc.`}
+                            title='JavaScript Algorithms and Data Structures'
+                            buttonText='Suivre le cours'
+                            link='/learn/javascript-algorithms-and-data-structures'
+                            description={`Alors que HTML et CSS contrôlent le contenu et le style d'une page,
+                          JavaScript est utilisé pour la rendre interactive. Dans le cadre du
+                          cours JavaScript Algorithm and Data Structures, tu apprendras
+                          les principes fondamentaux de JavaScript, etc.`}
                           />
                         </>
                       )}
-                    {paginatedData.map((course, index) => {
-                      if ('launch_url' in course) {
-                        if (course.long_description) {
-                          return (
-                            <PathCard
-                              key={course.name}
-                              icon={awsLogo}
-                              isAvailable={true}
-                              isSignedIn={isSignedIn}
-                              title={`${index + 1}. ${course.name}`}
-                              buttonText={`Suivre le cours`}
-                              link={course.launch_url}
-                              description={course.long_description}
-                              duration={convertTime(course.duration)}
-                              level={course.skill_level}
-                            />
-                          );
+                    {paginatedData.length > 0 ? (
+                      paginatedData.map((course, index) => {
+                        if ('launch_url' in course) {
+                          const firstCategory = course.category?.[0];
+                          const language =
+                            firstCategory?.tags?.[0]?.title || 'Unknown';
+
+                          if (course.long_description) {
+                            return (
+                              <PathCard
+                                language={language}
+                                key={course.name}
+                                icon={awsLogo}
+                                isAvailable={true}
+                                isSignedIn={isSignedIn}
+                                title={`${index + 1}. ${course.name}`}
+                                buttonText='Suivre le cours'
+                                link={course.launch_url}
+                                description={course.long_description}
+                                duration={convertTime(course.duration)}
+                                level={course.skill_level}
+                              />
+                            );
+                          } else {
+                            return (
+                              <CourseCard
+                                language={language}
+                                key={index.toString()}
+                                icon={awsLogo}
+                                isAvailable={true}
+                                isSignedIn={isSignedIn}
+                                title={`${index + 1}. ${course.name}`}
+                                buttonText='Suivre le cours'
+                                link={course.launch_url}
+                                description={course.short_description}
+                                duration={convertTime(course.duration)}
+                              />
+                            );
+                          }
                         } else {
                           return (
                             <CourseCard
-                              key={index}
-                              icon={awsLogo}
-                              isAvailable={true}
+                              language='French'
+                              key={`${index}-${course.id}`}
+                              icon='path/to/PhBookBookmark.png' // Remplacer par le chemin réel de l'image
+                              isAvailable={course.visible === 1}
                               isSignedIn={isSignedIn}
-                              title={`${index + 1}. ${course.name}`}
-                              buttonText={`Suivre le cours`}
-                              link={course.launch_url}
-                              description={course.short_description}
-                              duration={convertTime(course.duration)}
+                              title={course.displayname}
+                              buttonText='Suivre le cours'
+                              link={`${moodleBaseUrl}/course/view.php?id=${course.id}`}
+                              description={course.summary}
+                              duration={convertTime(course.timecreated)}
                             />
                           );
                         }
-                      } else {
-                        // Vérifie si le cours est un cours Raven
-                        return (
-                          <CourseCard
-                            key={index + course.id}
-                            icon={PhBookBookmark}
-                            phone={phone}
-                            name={name}
-                            badgeIcon={NewBadge}
-                            isAvailable={course.visible == 1}
-                            isSignedIn={isSignedIn}
-                            sameTab={true}
-                            external={true}
-                            title={course.displayname}
-                            buttonText={`Suivre le cours`}
-                            createAt={formatdate(course.timecreated)}
-                            link={`${moodleBaseUrl}/course/view.php?id=${course.id}`}
-                            description={course.summary}
-                          />
-                        );
-                      }
-                    })}
+                      })
+                    ) : (
+                      <div className='card-course-detail-container'>
+                        {renderCourseCardSkeletons(6)}
+                      </div>
+                    )}
                   </div>
-                ) : (
-                  <div className='card-course-detail-container'>
-                    {renderCourseCardSkeletons(6)}
-                  </div>
-                )}
+                ) : null}
+
+                <Spacer size={4} />
+                <div className='pagination-container'>
+                  <FontAwesomeIcon
+                    icon={faChevronLeft}
+                    className='pagination-chevron'
+                    onClick={() => onNavigueteBackward()}
+                  />
+                  <span className='pagination__number'>
+                    {currentPage}/{totalPages > 0 ? totalPages : 1}
+                  </span>
+                  <FontAwesomeIcon
+                    icon={faChevronRight}
+                    className='pagination-chevron'
+                    onClick={() => onNavigateForward()}
+                  />
+                </div>
               </div>
-            </div>
-            <Spacer size={4} />
-            <div className='pagination-container'>
-              <FontAwesomeIcon
-                icon={faChevronLeft}
-                className='pagination-chevron'
-                onClick={() => onNavigueteBackward()}
-              />
-              <span className='pagination__number'>
-                {currentPage}/{totalPages > 0 ? totalPages : 1}
-              </span>
-              <FontAwesomeIcon
-                icon={faChevronRight}
-                className='pagination-chevron'
-                onClick={() => onNavigateForward()}
-              />
             </div>
           </div>
         </main>
