ed9e776dcb83e8159d5b6756f052f70e7279d80a
Jumvi <judahmvi@gmail.com>
2024-09-16 10:38:26 +0100

    make objectno mutable

--- client/src/utils/routes.ts
+++ client/src/utils/routes.ts
@@ -1,47 +1,48 @@
-const routes = {
-  learningPath: {
+//Notons l'utilisation de object.freeze() afin de
+//rendre les objects immuables car on ne veut pas que ces propriétés change de valeurs.
+const routes = Object.freeze({
+  learningPath: Object.freeze({
     index: '/learning-path',
     aws: '/learning-path/aws-courses',
     moodle: '/learning-path/:category/:categoryId',
     fullstack: '/learning-path/developpement-web'
-  },
-  catalogue: {
+  }),
+  catalogue: Object.freeze({
     index: '/catalogue',
     catalogueTitle: '/catalogue/:value',
     aws: '/catalogue/amazon-web-service',
     programmation: '/catalogue/programmation',
     moodle: '/catalogue/:category'
-  }
-};
+  })
+});
 
-const allQuery = {
+const allQuery = Object.freeze({
   key: {
     level: 'level',
     duration: 'durée',
     language: 'langue',
     type: 'type'
   },
-
-  value: {
-    level: {
+  value: Object.freeze({
+    level: Object.freeze({
       debutant: 'Débutant',
       intermediaire: 'Intermediaire',
-      avancé: 'Avancé'
-    },
-    duration: {
+      avance: 'Avancé'
+    }),
+    duration: Object.freeze({
       lessOneHour: '>1h',
       overOneHour: '1>5h',
       overFiveHour: '>5h'
-    },
-    language: {
+    }),
+    language: Object.freeze({
       french: 'French',
       english: 'English'
-    },
-    type: {
+    }),
+    type: Object.freeze({
       cours: 'Cours',
       parcours: 'Parcours'
-    }
-  }
-};
+    })
+  })
+});
 
 export { routes, allQuery };
