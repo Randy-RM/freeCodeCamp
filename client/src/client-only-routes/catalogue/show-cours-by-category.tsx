d186968c4c2c5ec8a669ef2a5646cafcdf8aef35
Jumvi <judahmvi@gmail.com>
2024-09-17 14:15:32 +0100

    fix:fixed filter by language error

--- client/src/client-only-routes/catalogue/show-cours-by-category.tsx
+++ client/src/client-only-routes/catalogue/show-cours-by-category.tsx
@@ -280,7 +280,7 @@ function CourseByCatalogue(props: CoursesProps): JSX.Element {
         // Initialisation des flags de filtres
         const isLanguageFilterActive =
           currentUrl.includes(allQuery.value.language.english) ||
-          currentUrl.includes(allQuery.value.language.english);
+          currentUrl.includes(allQuery.value.language.french);
         const isTypeFilterActive =
           currentUrl.includes(allQuery.value.type.parcours) ||
           currentUrl.includes(allQuery.value.type.cours);
@@ -298,16 +298,18 @@ function CourseByCatalogue(props: CoursesProps): JSX.Element {
             allQuery.value.language.english
           );
           const filterByFrench = currentUrl.includes(
-            allQuery.value.language.english
+            allQuery.value.language.french
           );
 
           filteredRavenCourses = filteredRavenCourses.filter(course => {
             const courseLanguage = course.category?.[0]?.tags?.[0]?.title;
+            console.log('langue', courseLanguage);
+
             return (
               (filterByEnglish &&
-                courseLanguage === allQuery.value.language.english) ||
+                courseLanguage == allQuery.value.language.english) ||
               (filterByFrench &&
-                courseLanguage === allQuery.value.language.english)
+                courseLanguage == allQuery.value.language.french)
             );
           });
         }
