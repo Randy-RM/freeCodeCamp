d4c9a7a7631c4a7b88d34b33eb34d94ff28f18bc
Jumvi <judahmvi@gmail.com>
2024-09-18 12:29:00 +0100

    update fetch raven course

--- client/src/utils/ajax.ts
+++ client/src/utils/ajax.ts
@@ -143,18 +143,6 @@ export const dataForprogramation: ProgramationCourses[] = [
   }
 ];
 
-// <CourseCard
-//               level='Débutant'
-//               language='French'
-//               icon={AlgoIcon}
-//               alt=''
-//               isAvailable={true}
-//               title='JavaScript Algorithms and Data Structures'
-//               buttonText='Suivre le cours'
-//               link='/learn/javascript-algorithms-and-data-structures'
-//               description={`
-//     Ce cours t'enseigne les bases de JavaScript pour rendre les pages interactives, ainsi que les algorithmes et structures de données en JavaScript., etc.
-//   `}
 export const courseDescriptions: Record<CourseCategoryTitle, CourseDetails> = {
   Développement: {
     titre: 'Développement Web',
@@ -613,44 +601,34 @@ const { moodleApiBaseUrl, moodleApiToken, ravenAwsApiKey } = envData;
 const ravenLocalToken = getRavenTokenDataFromLocalStorage();
 
 export const getRavenResources = async (currentPage: number) => {
-  await getRavenToken();
-
-  const ravenData: RavenFetchCoursesDto = {
-    apiKey: ravenAwsApiKey,
-    token: ravenLocalToken?.token || '',
-    currentPage: currentPage,
-    fromDate: '01-01-2023',
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    valid_to: '06-24-2024'
-  };
-  const getReveanCourses = await getAwsCourses(ravenData);
+  const getReveanCourses = await getAwsCourses(currentPage);
 
   return getReveanCourses;
 };
 export const getRavenPathResources = async (currentPage: number) => {
-  await getRavenToken();
-
-  const ravenData: RavenFetchCoursesDto = {
-    apiKey: ravenAwsApiKey,
-    token: ravenLocalToken?.token || '',
-    currentPage: currentPage,
-    fromDate: '01-01-2023',
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    valid_to: '06-24-2024'
-  };
-  const getReveanPathCourses = await getAwsPath(ravenData);
+  const getReveanPathCourses = await getAwsPath(currentPage);
 
   return getReveanPathCourses;
 };
 
 //end getRavenResources
 
-export async function getAwsCourses(data: RavenFetchCoursesDto) {
+export async function getAwsCourses(currentPage: number) {
+  await getRavenToken();
+
+  const ravenData: RavenFetchCoursesDto = {
+    apiKey: ravenAwsApiKey,
+    token: ravenLocalToken?.token || '',
+    currentPage: currentPage,
+    fromDate: '01-01-2023',
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    valid_to: '06-24-2024'
+  };
   let response: unknown | RavenCourse[];
 
   try {
     response = await get(
-      `/get-raven-courses?awstoken=${data.token}&fromdate=${data.fromDate}&todate=${data.valid_to}`
+      `/get-raven-courses?awstoken=${ravenData.token}&fromdate=${ravenData.fromDate}&todate=${ravenData.valid_to}`
     );
   } catch (error) {
     response = null;
@@ -658,12 +636,22 @@ export async function getAwsCourses(data: RavenFetchCoursesDto) {
 
   return response;
 }
-export async function getAwsPath(data: RavenFetchCoursesDto) {
+export async function getAwsPath(currentPage: number) {
+  await getRavenToken();
+
+  const ravenData: RavenFetchCoursesDto = {
+    apiKey: ravenAwsApiKey,
+    token: ravenLocalToken?.token || '',
+    currentPage: currentPage,
+    fromDate: '01-01-2023',
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    valid_to: '06-24-2024'
+  };
   let response: unknown | RavenCourse[];
 
   try {
     response = await get(
-      `/get-raven-path?awstoken=${data.token}&fromdate=${data.fromDate}&todate=${data.valid_to}`
+      `/get-raven-path?awstoken=${ravenData.token}&fromdate=${ravenData.fromDate}&todate=${ravenData.valid_to}`
     );
   } catch (error) {
     response = null;
@@ -719,7 +707,6 @@ export async function getAwsPath(data: RavenFetchCoursesDto) {
     };
 
     const filteredCourses = filterCourses(response);
-
     return filteredCourses;
   }
 
