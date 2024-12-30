c5b503a52e6181d54ce5b2848a6da4aeec5dc69e
Jumvi <judahmvi@gmail.com>
2024-12-30 09:46:12 +0100

    feat: refactor course rendering to use SwhowIaCourses component for IA courses

--- client/src/client-only-routes/catalogue/all-course-by-type.tsx
+++ client/src/client-only-routes/catalogue/all-course-by-type.tsx
@@ -15,6 +15,7 @@ import {
 } from '../../utils/allFunctions';
 import envData from '../../../../config/env.json';
 import { allQuery } from '../../utils/routes';
+import SwhowIaCourses from './show-ia-courses';
 
 // Unification des types pour les cours
 type Course = RavenCourse | MoodleCourse | ProgramationCourses;
@@ -108,70 +109,9 @@ const CourseList: React.FC<CourseListProps> = ({ courses, valueOfUrl }) => {
               valueOfUrl === 'Intelligence%20 %20artificielle' ||
               valueOfUrl === 'intelligence artificielle'
             ) {
-              if ('visible' in course) {
-                const courseTyped = course;
-                return (
-                  <CourseCard
-                    key={index}
-                    language={courseTyped.langue}
-                    level={courseTyped.level}
-                    icon={PhBookBookmark}
-                    isAvailable={courseTyped.visible === 1}
-                    title={courseTyped.displayname}
-                    buttonText='Suivre le cours'
-                    link={`${moodleBaseUrl}/course/view.php?id=${courseTyped.id}`}
-                    description={courseTyped.summary}
-                    duration={convertTimestampToTime(courseTyped.duration)}
-                  />
-                );
-              }
-              const courseTyped = course as RavenCourse;
-              const firstCategory = courseTyped.category?.[0];
-              const language = firstCategory?.tags?.[0]?.title || 'Unknown';
-
-              if (courseTyped.long_description) {
-                return (
-                  <PathCard
-                    key={courseTyped.name}
-                    language={language}
-                    icon={awsLogo}
-                    isAvailable={true}
-                    title={`${index + 1}. ${courseTyped.name}`}
-                    buttonText='Suivre le parcours'
-                    link={courseTyped.launch_url}
-                    description={formatDescription(
-                      courseTyped.long_description
-                    )}
-                    duration={convertTime(courseTyped.duration)}
-                    level={
-                      courseTyped.skill_level === 'Fundamental'
-                        ? allQuery.value.level.debutant
-                        : ''
-                    }
-                  />
-                );
-              } else {
-                return (
-                  <CourseCard
-                    key={index.toString()}
-                    level={
-                      courseTyped.skill_level === 'Fundamental'
-                        ? allQuery.value.level.debutant
-                        : ''
-                    }
-                    language={language}
-                    icon={awsLogo}
-                    isAvailable={true}
-                    title={`${index + 1}. ${courseTyped.name}`}
-                    buttonText='Suivre le cours'
-                    link={courseTyped.launch_url}
-                    description={formatDescription(
-                      courseTyped.short_description
-                    )}
-                    duration={convertTime(courseTyped.duration)}
-                  />
-                );
-              }
+              return (
+                <SwhowIaCourses courses={courses} valueOfUrl={valueOfUrl} />
+              );
             }
             // Gestion des cours Moodle
             const courseTyped = course as MoodleCourse;
