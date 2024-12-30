d00e9e41d80e58421b6d2c9e7a40d7759873ba9f
Jumvi <judahmvi@gmail.com>
2024-12-30 10:46:03 +0100

    feat: add ShowOnlyRavenCourses component for rendering AWS courses

---
+++ client/src/client-only-routes/catalogue/show-only-raven-courses.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import CourseCard from '../../components/CourseCard/course-card';
+import PathCard from '../../components/PathCard/path-card';
+import { RavenCourse, MoodleCourse } from '../show-courses';
+import { ProgramationCourses } from '../../utils/ajax';
+import { renderCourseCardSkeletons } from '../../components/helpers';
+import awsLogo from '../../assets/images/aws-logo.png';
+import { convertTime, formatDescription } from '../../utils/allFunctions';
+import { allQuery } from '../../utils/routes';
+
+// Unification des types pour les cours
+type Course = RavenCourse | MoodleCourse | ProgramationCourses;
+
+interface CourseListProps {
+  courses: Course[]; // Tableau contenant différents types de cours
+  valueOfUrl: string; // Url de la catégorie pour filtrer les cours
+}
+
+const ShowOnlyRavenCouses: React.FC<CourseListProps> = ({
+  courses,
+  valueOfUrl
+}) => {
+  return (
+    <>
+      {courses.length === 0
+        ? renderCourseCardSkeletons(6)
+        : courses.map((course, index) => {
+            if (valueOfUrl === 'amazon web service') {
+              const courseTyped = course as RavenCourse;
+              const firstCategory = courseTyped.category?.[0];
+              const language = firstCategory?.tags?.[0]?.title || 'Unknown';
+
+              if (courseTyped.long_description) {
+                return (
+                  <PathCard
+                    key={courseTyped.name}
+                    language={language}
+                    icon={awsLogo}
+                    isAvailable={true}
+                    title={`${index + 1}. ${courseTyped.name}`}
+                    buttonText='Suivre le parcours'
+                    link={courseTyped.launch_url}
+                    description={formatDescription(
+                      courseTyped.long_description
+                    )}
+                    duration={convertTime(courseTyped.duration)}
+                    level={
+                      courseTyped.skill_level === 'Fundamental'
+                        ? allQuery.value.level.debutant
+                        : ''
+                    }
+                  />
+                );
+              } else {
+                return (
+                  <CourseCard
+                    key={index.toString()}
+                    level={
+                      courseTyped.skill_level === 'Fundamental'
+                        ? allQuery.value.level.debutant
+                        : ''
+                    }
+                    language={language}
+                    icon={awsLogo}
+                    isAvailable={true}
+                    title={`${index + 1}. ${courseTyped.name}`}
+                    buttonText='Suivre le cours'
+                    link={courseTyped.launch_url}
+                    description={formatDescription(
+                      courseTyped.short_description
+                    )}
+                    duration={convertTime(courseTyped.duration)}
+                  />
+                );
+              }
+            }
+          })}
+    </>
+  );
+};
+
+export default ShowOnlyRavenCouses;
