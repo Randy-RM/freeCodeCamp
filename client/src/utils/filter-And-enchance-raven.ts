bf473cb25b19b6cf2cf3987496e89244a9c23730
Jumvi <judahmvi@gmail.com>
2025-02-05 11:35:09 +0100

    feat: add filterAndEnhanceCourses utility to process RavenCourse data

---
+++ client/src/utils/filter-And-enchance-raven.ts
@@ -0,0 +1,21 @@
+import { RavenCourse } from '../client-only-routes/show-courses';
+
+export function filterAndEnhanceCourses(courses: RavenCourse[]): RavenCourse[] {
+  return courses
+    .filter(course =>
+      course.category?.some(cat =>
+        cat.tags?.some(tag => /English|French/.test(tag.title))
+      )
+    )
+    .map(course => {
+      return {
+        ...course,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        skill_level:
+          course.category?.find(cat =>
+            cat.tags?.some(tag => tag.title === 'Skill Level')
+          )?.tags?.[0]?.title || 'Fundamental',
+        roles: course.category?.[1]?.tags?.[0]?.title || ''
+      };
+    });
+}
