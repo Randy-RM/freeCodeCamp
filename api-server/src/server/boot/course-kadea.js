fc71e614ee1b5db7fdb653004e12cf2fe9a182cc
Jumvi <judahmvi@gmail.com>
2025-02-06 12:11:09 +0100

    feat: add Kadea courses data for Responsive Web Design and JavaScript Algorithms

---
+++ api-server/src/server/boot/course-kadea.js
@@ -0,0 +1,36 @@
+export const coursesKAdea = [
+  {
+    title: 'Responsive Web Design',
+    level: 'debutant',
+    language: 'French',
+    sponsorIcon: 'LaediesActIcon',
+    alt: 'Icone ladies Act des cours Responsive design',
+    isAvailable: true,
+    link: '/learn/responsive-web-design/',
+    description:
+      "Ce cours t'apprend les langages HTML pour le contenu et CSS pour la conception, ainsi que la création de pages Web adaptatives pour différentes tailles d'écran.",
+    duration: 120,
+    type: 'Cours',
+    specification: 'Responsive Web Design',
+    enrolementCount: 0,
+    author: 'kadea',
+    category: ['Web Development']
+  },
+  {
+    title: 'JavaScript Algorithms and Data Structures',
+    level: 'debutant',
+    language: 'French',
+    sponsorIcon: 'AlgoIcon',
+    alt: 'Icone cours algorithm et structure de données',
+    isAvailable: true,
+    link: '/learn/javascript-algorithms-and-data-structures',
+    description:
+      "Ce cours t'enseigne les bases de JavaScript pour rendre les pages interactives, ainsi que les algorithmes et structures de données en JavaScript, etc.",
+    duration: 120,
+    type: 'Cours',
+    specification: 'JavaScript Algorithms and Data Structures',
+    enrolementCount: 0,
+    author: 'kadea',
+    category: ['Programming']
+  }
+];
